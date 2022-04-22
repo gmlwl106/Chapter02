dd2f7f6b107dc288a4e9293ed95f49b835c10fb1
gmlwl106 <66778894@naver.com>
2022-04-22 12:19:12 +0900

    Chapter02. 연습문제-PointApp

--- src/com/javaex/ex04/Point.java
+++ src/com/javaex/ex04/Point.java
@@ -11,26 +11,33 @@ public class Point {
 	
 	
 	//메소드 - gs
-	//Setter
-	public void setX(int pointX) {
-		x = pointX;
-	}
-	public void setY(int pointY) {
-		y = pointY;
-	}
-	
-	//Getter
+
 	public int getX() {
 		return x;
 	}
+
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+
 	public int getY() {
 		return y;
 	}
+
+
+	public void setY(int y) {
+		this.y = y;
+	}
+	
 	
 	//메소드 - 일반
 	public void draw() {
 		System.out.println("점[x=" + x + ", y=" + y + "]을 그렸습니다.");
 	}
+
+
 	
 	
 }
