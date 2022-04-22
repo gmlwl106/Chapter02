dc6909f27205d7555f47f13b7ad811e90dce1e01
gmlwl106 <66778894@naver.com>
2022-04-22 18:38:03 +0900

    Chapter02. 연습문제-PointApp

--- src/com/javaex/ex04/PointApp.java
+++ src/com/javaex/ex04/PointApp.java
@@ -6,11 +6,20 @@ public class PointApp {
 		Point p1 = new Point();
 		p1.setX(5);
 		p1.setY(5);
-		p1.draw();
 		
 		Point p2 = new Point();
 		p2.setX(10);
 		p2.setY(23);
+		
+		Point p3 = new Point(10, 20);
+		
+		Point p4 = new Point(100);
+		p4.setY(200);
+		
+		
+		p1.draw();
 		p2.draw();
+		p3.draw(true);
+		p4.draw(false);
 	}
 }
