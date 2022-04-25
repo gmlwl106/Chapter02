387b31b6856bfff7fa6e3d41b2e2dfa5c3ce0514
gmlwl106 <66778894@naver.com>
2022-04-25 13:42:09 +0900

    Chapter02. static

--- src/com/javaex/ex10/GoodsApp.java
+++ src/com/javaex/ex10/GoodsApp.java
@@ -1,7 +1,12 @@
 package com.javaex.ex10;
 
+//static 사전조사
 public class GoodsApp {
-	//static 사전조사
+	//필드
+	//생성자
+	//메소드gs
+	//메소드 일반
+	
 	public static void main(String[] args) {
 		
 		System.out.println(Goods.count);
